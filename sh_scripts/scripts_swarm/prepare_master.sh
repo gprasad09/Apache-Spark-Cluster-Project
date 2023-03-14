d58e74f874c48f62a7c089f7e3e63f963743bb28
gprasad09 <98559222+gprasad09@users.noreply.github.com>
2023-03-14 17:58:57 -0400

    Update prepare_master.sh

--- sh_scripts/scripts_swarm/prepare_master.sh
+++ sh_scripts/scripts_swarm/prepare_master.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 manager=$(./sh_scripts/scripts_swarm/get_manager_id.sh)     #get container id of master
 
+echo "Configure manager container ID : $manager"
+
 cd ~/Apache-Spark-Cluster-Project/config_files
 
 docker cp $manager:/root/.bashrc ./bashrc.tmp               #copy ~/.bashrc from the container to host machine

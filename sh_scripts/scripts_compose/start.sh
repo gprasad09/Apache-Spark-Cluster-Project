5374ed7b0e1464aba7350929c8cba69bfe7192e8
gprasad09 <prasad.girraj@gmail.com>
2023-04-10 22:00:44 -0400

    Rearranged folders

--- sh_scripts/scripts_compose/start.sh
+++ sh_scripts/scripts_compose/start.sh
@@ -1,5 +1,5 @@
-!bin/bash
+#!/bin/bash
 
-docker compose -f ~/Apache-Spark-Cluster-Project/docker_files/docker-compose_master.yml  start
+docker compose -f ~/Apache-Spark-Cluster-Project/docker_files/docker_composefiles/docker-compose_master.yml start
 
 ~/Apache-Spark-Cluster-Project/sh_scripts/scripts_compose/start_spark.sh
--- sh_scripts/scripts_compose/stop.sh
+++ sh_scripts/scripts_compose/stop.sh
@@ -1,6 +1,6 @@
-#!bin/bash
+#!/bin/bash
 
 ~/Apache-Spark-Cluster-Project/sh_scripts/scripts_compose/stop_spark.sh
 
-docker compose -f ~/Apache-Spark-Cluster-Project/docker_files/docker-compose_master.yml stop
+docker compose -f ~/Apache-Spark-Cluster-Project/docker_files/docker_composefiles/docker-compose_master.yml stop
 
